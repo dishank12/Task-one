2101790324a05562e18481f7b014099c825f7608
Dishank Agrawal <149586822+dishank12@users.noreply.github.com>
2025-08-05 18:18:35 +0530

    Add files via upload

---
+++ Task/Task1.py
@@ -0,0 +1,15 @@
+# Code One
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("The number is positive.")
+else:
+    print("The number is negative or zero.")
+
+# Code Two 
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
